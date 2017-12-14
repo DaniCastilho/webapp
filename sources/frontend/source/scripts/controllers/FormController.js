8b812aee2862ba296bf5c0b1315c7526177efbf0
Dani Castilho <Daniele.Castilho@wunderman.com>
2017-12-14 17:41:33 -0200

    services e controllers em desenvolvimento

--- sources/frontend/source/scripts/controllers/FormController.js
+++ sources/frontend/source/scripts/controllers/FormController.js
@@ -4,12 +4,79 @@ class FormController{
 
     constructor(){
         this.service = new AppService();
-        this.buildFields();
+        this.getFields();
     }
 
-    buildFields(){
-        console.log(this.service.get)
+    getFields(){
+       this.service.get(
+           (response) => { this.onSuccess(response); },
+           (fail) => { this.onFail(fail); }
+       )
+
+       return
     }
+
+    onSuccess(response){
+       this.buildForm(response)
+    }
+
+    onFail(fail){
+        console.log(fail)
+    }
+
+    buildForm(fields){
+
+        let _field;
+        let _container = document.getElementsByClassName('section__content')[0];
+        let _form = document.createElement("form");
+        _form.setAttribute("id", "form");
+        _container.appendChild(_form);
+        
+        for(_field of fields){
+            let _input = document.createElement("input");
+            
+            _input.setAttribute("type", _field.type);
+            _input.setAttribute("id", _field.id);
+            _input.setAttribute("name", _field.name);
+            _input.setAttribute("placeholder", _field.placeholder);
+            
+            _form.appendChild(_input);
+        }
+    }
+
+    setListeners() {
+       let _form = document.getElementById("form");
+
+       _form.addEventListener("submit", (event)=>{
+           event.preventDefault();
+           this.sendUserData(event);
+       })
+    }
+    sendUserData(){
+        let _name, _cpf, _phone, _address, _file;
+
+        _name = document.getElementById("txtFullname").value;
+        _cpf = document.getElementById("txtCPF").value;
+        _phone = document.getElementById("txtPhone").value;
+        _address = document.getElementById("txtAddress").value;
+        _file = document.getElementById("txtFile").value;
+
+        console.log({
+            "nome": _name,
+            "cpf": _cpf,
+            "phone": _phone,
+            "address": _address,
+            "file": _file
+        })
+        this.service.saveData({
+            "nome" : _name,
+            "cpf" : _cpf,
+            "phone" : _phone,
+            "address" : _address,
+            "file" : _file
+        })
+    }
+
 
 
 }
--- sources/frontend/source/scripts/services/AppService.js
+++ sources/frontend/source/scripts/services/AppService.js
@@ -5,12 +5,29 @@ class AppService{
         this.ENDPOINT_FIELDS = "fields"
     }
 
-    get(){
+    get(success, fail){
 
-        fetch(`${this.BASE_URL}${this.ENDPOINT_FIELDS}`)
-            .then(function(response) {
-            return response
+        fetch(`${this.BASE_URL}${this.ENDPOINT_FIELDS}`,{
+            method: 'get'
         })
+        .then(function(response) {
+               response.json().then(function(data){
+                  success(data)
+               })
+        })
+        .catch(function(error){
+            console.error('Failed retrieving information', err);
+        })
+    }
+
+    saveData(userData){
+        let _storage = localStorage;
+        console.log(userData)
+        // localStorage.setItem(userData.name);
+        // localStorage.setItem(userData.cpf);
+        // localStorage.setItem(userData.telefone);
+        // localStorage.setItem(userData.endereco);
+        // localStorage.setItem(userData.file);
     }
 }
 export default AppService;
