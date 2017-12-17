c79a1012947104f0333289319580208b9ff819f4
Dani Castilho <danicastilhodev@gmail.com>
2017-12-17 18:07:49 -0200

    estilizando

--- sources/frontend/source/scripts/controllers/FormController.js
+++ sources/frontend/source/scripts/controllers/FormController.js
@@ -2,73 +2,74 @@ import AppService from '../services/AppService.js';
 import DataBaseService from '../services/DataBaseService.js'
 import MapController from '../controllers/MapController.js'
 
-class FormController{
+class FormController {
 
-    constructor(){
+    constructor() {
         this.service = new AppService();
         this.dataBase = new DataBaseService();
         this.map = new MapController();
         this.getFields();
     }
 
-    getFields(){
-       this.service.get(
-           (response) => { this.onSuccess(response); },
-           (fail) => { this.onFail(fail); }
-       )
+    getFields() {
+        this.service.get(
+            (response) => { this.onSuccess(response); },
+            (fail) => { this.onFail(fail); }
+        )
 
-       return
+        return
     }
 
-    onSuccess(response){
-       this.buildForm(response)
+    onSuccess(response) {
+        this.buildForm(response)
     }
 
-    onFail(fail){
+    onFail(fail) {
         console.log(fail)
     }
 
-    buildForm(fields){
+    buildForm(fields) {
 
         let _field;
         let _container = document.getElementsByClassName('section__content')[0];
         let _form = document.createElement("form");
         _form.setAttribute("id", "form");
         _container.appendChild(_form);
-        
+
         let _map = document.createElement("div");
         _map.setAttribute("id", "map");
-        
-        for(_field of fields){
+
+        for (_field of fields) {
             let _input = document.createElement("input");
-            
+
             _input.setAttribute("type", _field.type);
             _input.setAttribute("id", _field.id);
             _input.setAttribute("name", _field.name);
             _input.setAttribute("placeholder", _field.placeholder);
-            
-            if(_field.type == "address"){
-                _form.appendChild(_map)
-                
+
+            _form.appendChild(_input);
+
+
+            if (_field.type == "address") {
+                _form.appendChild(_map); 
             }
             
-            _form.appendChild(_input);
         }
-        
+
         this.setListeners();
         this.map.initMap();
-        
+
     }
 
     setListeners() {
-       let _form = document.getElementById("form");
-        
-       _form.addEventListener("submit", (e)=>{
-           e.preventDefault();
-           this.sendUserData(event);
-       })
+        let _form = document.getElementById("form");
+
+        _form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            this.sendUserData(event);
+        })
     }
-    sendUserData(){
+    sendUserData() {
         let _name, _cpf, _phone, _address, _file;
 
         _name = document.getElementById("txtFullname").value;
@@ -77,13 +78,13 @@ class FormController{
         _address = document.getElementById("txtAddress").value;
         _file = document.getElementById("uplImage").value;
 
-        
+
         this.dataBase.saveData({
-            "name" : _name,
-            "cpf" : _cpf,
-            "phone" : _phone,
-            "address" : _address,
-            "file" : _file
+            "name": _name,
+            "cpf": _cpf,
+            "phone": _phone,
+            "address": _address,
+            "file": _file
         })
     }
 
